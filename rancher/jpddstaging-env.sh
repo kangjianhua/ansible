34a46a5a2a0568d96cfc1e97577de2946d9ec42b
kevin.kang <kevin@ptmind.com>
2018-12-15 11:57:53 +0900

    add reindex gateway

---
+++ rancher/jpddstaging-env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export RANCHER_URL=https://jpcaas.ptmind.com/v1/projects/1a28
+export RANCHER_ACCESS_KEY=1347873B1164798E770C
+export RANCHER_SECRET_KEY=Xn6U3GN8uUm7n8Y5SsBsayTbShCQ9N7jpsBYPSVz
+export RANCHER_CLIENT_DEBUG=true
